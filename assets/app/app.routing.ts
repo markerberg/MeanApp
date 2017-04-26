73deb8942dc6e8b40d94de96208270e634e9215a
Regena Goberdhan <Regena Goberdhan>
2017-04-25 21:52:23 -0400

    add new routing for auth

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -2,12 +2,14 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
+import { AUTH_ROUTES } from "./auth/auth.routes";
 
 const APP_ROUTES: Routes = [
 	// pathMatch tells ang2 to only redirect if '' is empty. we must include this for '' path
 	{ path: '', redirectTo: '/messages', pathMatch: 'full' },
 	{ path: 'messages', component: MessagesComponent },
-	{ path: 'auth', component: AuthenticationComponent }
+	// register auth.routes array as children for /auth 
+	{ path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
 ];
 
 // this register our routes in ang2 router module
