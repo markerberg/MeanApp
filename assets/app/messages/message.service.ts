62d81494552a623596e548daf52087e6f2f967bc
Regena Goberdhan <Regena Goberdhan>
2017-05-14 02:46:52 -0400

    update message service component

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -17,14 +17,18 @@ export class MessageService{
 
 	// SAVE MESSAGE
 	addMessage(message: Message) {
-		this.messages.push(message);
 		const body = JSON.stringify(message);
 		const headers = new Headers({'Content-Type': 'application/json'});// change headers w/ang2
 		// .map allow us to transform data once it comes back from server
 		// we set up how data should transform here, so funcs can use this transformed format
 		// we then configure req headers to have the headers we set 
 		return this.http.post('http://localhost:3000/message', body, {headers: headers})
-			.map((response: Response) => response.json()) // give us data attach from response in json
+			.map((response: Response) => {
+				var result = response.json()  // give us data attach from response in json
+				const message = new Message(result.obj.content, 'Dummy', result.obj._id, null); // created msg is stored in obj field on backend
+				this.messages.push(message);
+				return message;
+			})
 			// we return observable, map func converts to observable automatically but catch doesn't, so we call it
 			.catch((error: Response) => Observable.throw(error.json())); // extract data obj from error response
 	}
@@ -40,7 +44,7 @@ export class MessageService{
 				// loop through messages array from server, transform and push into new transformed array
 				for (let message of messages) { //forOf is es6 syntax supported by ts to loop through const messages 
 					// assign name/value based on message model
-					transformedMessages.push(new Message(message.content, 'TestName', message.id, null));
+					transformedMessages.push(new Message(message.content, 'TestName', message._id, null));
 				}
 				this.messages = transformedMessages;
 				return transformedMessages;
@@ -54,7 +58,11 @@ export class MessageService{
 	}
 
 	updateMessage(message: Message) {
-		
+		const body = JSON.stringify(message);
+		const headers = new Headers({'Content-Type': 'application/json'});
+		return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
+			.map((response: Response) => response.json()) 
+			.catch((error: Response) => Observable.throw(error.json())); 
 	}
 
 	deleteMessage(message: Message) {
--- routes/messages.js
+++ routes/messages.js
@@ -40,4 +40,36 @@ router.post('/', function (req, res, next) {
 	});
 });
 
+router.patch('/:id', function(req, res, next) {
+	// find message based on ID
+	Message.findById(req.params.id, function(err, message){
+		if (err) {
+			return res.status(500).json({
+				title: 'An error occured',
+				error: err
+			}); 
+		}
+		if (!Message) {
+			return res.status(500).json({
+				title: 'No message found',
+				error: {message: 'Message not found'}
+			});
+		}
+		// update the message.content which is the field we're changing
+		message.content = req.body.content;
+		message.save(function(err, result){
+		if(err) {
+			return res.status(500).json({
+				title: 'An error occured',
+				error: err
+			});
+		}
+		res.status(200).json({
+			message: 'Updated message',
+			obj: result
+		});
+	});
+	});
+});
+
 module.exports = router;
