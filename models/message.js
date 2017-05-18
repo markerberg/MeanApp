33f219c1212c8eb2d8a11bf34041d94df65bc161
Regena Goberdhan <Regena Goberdhan>
2017-05-18 19:48:12 -0400

    update messaging

--- models/message.js
+++ models/message.js
@@ -1,9 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var User = require('../user');
+
 var schema = new Schema({
 	content: { type: String, required: true },
 	user: { type: Schema.Types.ObjectId, ref: 'User' }
+});
+
+//mongoose middleware execute when something happens to message
+schema.post('remove', function(message){
+	// for message we remove, find its user
+	User.findById(message.user, function(err, user){
+		user.messages.pull(message); // pull this message from user array
+		user.save();
+	});
 });
 
 // the first arg lets us use new Message and determine collection name(messages)
--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
+var User = require('../models/user');
 var Message = require('../models/message');
 
 router.get('/', function(req, res, next) {
@@ -19,23 +21,49 @@ router.get('/', function(req, res, next) {
 		});
 });
 
-router.post('/', function (req, res, next) {
-	var message = new Message({
-		content: req.body.content
-	});
-	message.save(function(err, result){
-		if(err) {
-			// we return here so that we send a res and the status(201) doesnt get sent aswell 
-			return res.status(500).json({
-				title: 'An error occured',
+router.use('/', function(req, res, next) {
+	// validate incoming token with jwt looking at query params
+	jwt.verify(req.query.token, 'secret', function(err, decoded) {
+		if (err) {
+			return res.status(401).json({
+				title: 'Not Authenticated',
 				error: err
 			});
 		}
-		// func ends here, so dont return here bc the code after it wouldnt execute. it ends naturally 
-		res.status(201).json({
-			message: 'Saved message',
-			// obj is the saved message from db
-			obj: result
+		next();
+	})
+});
+
+router.post('/', function (req, res, next) {
+	var decoded = jwt.decode(req.query.token);
+	User.findById(decoded.user._id, function(err, user){
+			if(err) {
+				return res.status(500).json({
+					title: 'An error occured',
+					error: err
+				});
+			}
+			var message = new Message({
+			content: req.body.content,
+			user: user // set it to user we extract from db
+		});
+		message.save(function(err, result){
+			if(err) {
+				// we return here so that we send a res and the status(201) doesnt get sent aswell 
+				return res.status(500).json({
+					title: 'An error occured',
+					error: err
+				});
+			}
+			// when saved, access message arr of user and push new message to array 
+			user.messages.push(result);
+			user.save();
+			// func ends here, so dont return here bc the code after it wouldnt execute. it ends naturally 
+			res.status(201).json({
+				message: 'Saved message',
+				// obj is the saved message from db
+				obj: result
+			});
 		});
 	});
 });
